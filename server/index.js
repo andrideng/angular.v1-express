a397aebf2bedf5161061d6b696dd7280ec0fbe60
Andri Deng <andrideng@gmail.com>
2017-07-14 15:31:29 +0700

    fix minor bug

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use('/api', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     console.log('err')
-    var err = new Error('Not Found');
+    var err = new Error('API is ready in /api');
     err.status = 404;
     next(err);
 });
