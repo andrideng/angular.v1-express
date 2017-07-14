61606fca114f6a89740c78f4fb9c28c2ab90a7a4
Andri Deng <andrideng@gmail.com>
2017-07-14 15:20:50 +0700

    fit with heroku settings

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -3,10 +3,10 @@ var env = require('dotenv').config().parsed;
 
 var connection = mysql.createPool({
 
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASS,
-    database: env.DB_NAME
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 
 });
 module.exports = connection;
